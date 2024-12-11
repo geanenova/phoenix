af555e37bb59ed17a27b5edd3446e7b3453823d5
gean dos reis chaves <129519417+geanenova@users.noreply.github.com>
2024-12-11 19:13:14 -0300

    Add files via upload

---
+++ start_miner.sh
@@ -0,0 +1,6 @@
+#
+# Example shell file for starting PhoenixMiner.exe to mine ETC
+#
+
+# IMPORTANT: Replace the ETC address with your own ETC wallet address in the -wal option (Rig001 is the name of the rig)
+./PhoenixMiner -pool ssl://eu1-etc.ethermine.org:5555 -wal 0x4b96A2B3129CeE730d649c26EbA9Fb54FA0a1Af8.Rig001 -coin etc
