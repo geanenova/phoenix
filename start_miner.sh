8213e2ba3d3659e15d523623fb9eee9c0f49f53f
gean dos reis chaves <129519417+geanenova@users.noreply.github.com>
2024-12-11 20:46:32 -0300

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -3,4 +3,4 @@
 #
 
 # IMPORTANT: Replace the ETC address with your own ETC wallet address in the -wal option (Rig001 is the name of the rig)
-./PhoenixMiner -pool stratum+tcp://bch.poolbinance.com:8888 -wal gean3e324.Rig003 -pass 123456
+./PhoenixMiner -pool stratum+tcp://etc.poolbinance.com:1800 -wal gean3e324.Rig003 -pass 123456
